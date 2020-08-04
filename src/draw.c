1377ee411e450af22c14e955cc4afb30ff91740b
Qi Dang <qdang@e1z2r2p4.42.us.org>
2020-08-03 18:22:45 -0700

    1/2 sight is done

--- src/draw.c
+++ src/draw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qdang <qdang@student.42.us.org>            +#+  +:+       +#+        */
+/*   By: qdang <qdang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/27 14:53:54 by qdang             #+#    #+#             */
-/*   Updated: 2020/08/03 12:21:33 by qdang            ###   ########.fr       */
+/*   Updated: 2020/08/03 18:12:44 by qdang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,18 +43,22 @@ void	intersect(t_info *s, double radian)
 	int		grid_x;
 	int		grid_y;
 	
+	int		temp_x;
+	int		temp_y;
+	
 	i = 0;
 	j = 0;
-	grid_x = s->start_y + s->move_y;
-	grid_y = s->start_x + s->move_x;
-
+	
+	grid_x = s->start_x + s->move_x;	
+	grid_y = s->start_y + s->move_y;
+	
 	if (radian == 0)
 	{
-		c = s->map[grid_x - 1][grid_y];
+		c = s->map[grid_y - 1][grid_x];
 		while (c == '0' || c == s->start)
 		{
 			i++;
-			c = s->map[grid_x - 1 - i][grid_y];
+			c = s->map[grid_y - 1 - i][grid_x];
 		}
 		s->its_x = s->stand_x;
 		s->its_y = s->stand_y - (int)((0.5 + i) * UL);
@@ -62,17 +66,33 @@ void	intersect(t_info *s, double radian)
 	
 	if (radian > 0 && radian < M_PI_2)
     {
-		c = s->map[grid_x - 1][grid_y + (int)(0.5 + 0.5 * tan(radian))];
+		temp_y = grid_y - 1 - i;
+		temp_x = grid_x + (int)(0.5 + (0.5 + i) * tan(radian));
+		if (temp_y >= 0 && temp_x <= s->map_x - 1)
+			c = s->map[temp_y][temp_x];
 		while (c == '0' || c == s->start)
 		{
 			i++;
-			c = s->map[grid_x - 1 - i][grid_y + (int)(0.5 + (0.5 + i) * tan(radian))];
+			temp_y = grid_y - 1 - i;
+			temp_x = grid_x + (int)(0.5 + (0.5 + i) * tan(radian));
+			if (temp_y >= 0 && temp_x <= s->map_x - 1)
+				c = s->map[temp_y][temp_x];
+			else
+				break;				
 		}
-		c = s->map[grid_x - (int)(0.5 + 0.5 / tan(radian))][grid_y + 1];
+		temp_y = grid_y - (int)(0.5 + (0.5 + j) / tan(radian));
+		temp_x = grid_x + 1 + j;
+		if (temp_y >= 0 && temp_x <= s->map_x - 1)
+			c = s->map[temp_y][temp_x];	
 		while (c == '0' || c == s->start)
 		{
 			j++;
-			c = s->map[grid_x - (int)(0.5 + (0.5 + j) / tan(radian))][grid_y + 1 + j];
+			temp_y = grid_y - (int)(0.5 + (0.5 + j) / tan(radian));
+			temp_x = grid_x + 1 + j;
+			if (temp_y >= 0 && temp_x <= s->map_x - 1)
+				c = s->map[temp_y][temp_x];			
+			else
+				break;
 		}
 		if ((int)((0.5 + i) * UL) <= (int)((0.5 + j) * UL / tan(radian)))
 		{
@@ -81,49 +101,70 @@ void	intersect(t_info *s, double radian)
 		}
 		else
 		{
-			s->its_x = s->stand_x + (int)((0.5 + j) * UL - 1);		
+			s->its_x = s->stand_x + (int)((0.5 + j) * UL);		
 			s->its_y = s->stand_y - (int)((0.5 + j) * UL / tan(radian));
 		}
 	}
 
 	if (radian == M_PI_2)
 	{
-		c = s->map[grid_x][grid_y + 1];
+		c = s->map[grid_y][grid_x + 1];
 		while (c == '0' || c == s->start)
 		{
 			i++;
-			c = s->map[grid_x][grid_y + 1 + i];
+			c = s->map[grid_y][grid_x + 1 + i];
 		}
 		s->its_x = s->stand_x + (int)((0.5 + i) * UL - 1);
 		s->its_y = s->stand_y;	
 	}
-/*	
+
+
 	if (radian > M_PI_2 && radian < M_PI)
 	{
-		c = s->map[grid_x + (int)(0.5 - 0.5 / tan(radian))][grid_y + 1];
+		radian -= M_PI_2;
+
+		temp_y = grid_y + (int)(0.5 + (0.5 + i) * tan(radian));
+		temp_x = grid_x + 1 + i;
+		if (temp_y <= s->map_y - 1 && temp_x <= s->map_x - 1)
+			c = s->map[temp_y][temp_x];
 		while (c == '0' || c == s->start)
 		{
 			i++;
-			c = s->map[grid_x + (int)(0.5 - (0.5 + i) / tan(radian))][grid_y + 1 + i];
+			temp_y = grid_y + (int)(0.5 + (0.5 + i) * tan(radian));
+			temp_x = grid_x + 1 + i;
+			if (temp_y <= s->map_y - 1 && temp_x <= s->map_x - 1)
+				c = s->map[temp_y][temp_x];
+			else
+				break;			
 		}
-		c = s->map[grid_x + 1][grid_y + (int)(0.5 - 0.5 * tan(radian))];
+
+		temp_y = grid_y + 1 + j;
+		temp_x = grid_x + (int)(0.5 + (0.5 + j) / tan(radian));
+		if (temp_y <= s->map_y - 1 && temp_x <= s->map_x - 1)
+			c = s->map[temp_y][temp_x];
 		while (c == '0' || c == s->start)
 		{
 			j++;
-			c = s->map[grid_x + 1 + j][grid_y + (int)(0.5 - (0.5 + j) * tan(radian))];
+			temp_y = grid_y + 1 + j;
+			temp_x = grid_x + (int)(0.5 + (0.5 + j) / tan(radian));
+			if (temp_y <= s->map_y - 1 && temp_x <= s->map_x - 1)
+				c = s->map[temp_y][temp_x];
+			else
+				break;
 		}
+
 		
 		if ((int)((0.5 + i) * UL) <= (int)((0.5 + j) * UL / tan(radian)))
 		{
-			s->its_x = s->stand_x + (int)((0.5 + i) * UL * tan(radian));
-			s->its_y = s->stand_y - (int)((0.5 + i) * UL);
+			s->its_x = s->stand_x + (int)((0.5 + i) * UL) ;
+			s->its_y = s->stand_y + (int)((0.5 + i) * UL * tan(radian));
 		}
 		else
 		{
-			s->its_x = s->stand_x + (int)((0.5 + j) * UL - 1);		
-			s->its_y = s->stand_y - (int)((0.5 + j) * UL / tan(radian));
+			s->its_x = s->stand_x + (int)((0.5 + j) * UL / tan(radian));		
+			s->its_y = s->stand_y + (int)((0.5 + j) * UL);
 		}
-*/		
+	}
 }
 
 /*
@@ -136,6 +177,28 @@ void    draw_2d_map(t_info *s)
     int     j;
 
 	mlx_clear_window(s->mlx_ptr, s->win_ptr);
+
+	s->stand_x = BEGIN_X + (s->start_x + s->move_x + 0.5) * UL;
+	s->stand_y = BEGIN_Y + (s->start_y + s->move_y + 0.5) * UL;
+/*
+	i = -1;
+	while (++i <= 1000)
+	{
+		intersect(s, (double)(0.001 * i));
+		draw_line(s, s->its_x, s->its_y, RED);
+	}
+*/
+
+	intersect(s, 1);
+	draw_line(s, s->its_x, s->its_y, RED);
+	intersect(s, 1.5);
+	draw_line(s, s->its_x, s->its_y, RED);
+	intersect(s, 2);
+	draw_line(s, s->its_x, s->its_y, RED);
+	intersect(s, 2.5);
+	draw_line(s, s->its_x, s->its_y, RED);	
+
+
     i = -1;
     while (++i < s->map_y)
     {
@@ -148,22 +211,9 @@ void    draw_2d_map(t_info *s)
                 draw_2d_block(s, BEGIN_X + j * UL, BEGIN_Y + i * UL, YELLOW);
         }
     }
-	s->stand_x = BEGIN_X + (s->start_x + s->move_x + 0.5) * UL;
-	s->stand_y = BEGIN_Y + (s->start_y + s->move_y + 0.5) * UL;
-	mlx_pixel_put(s->mlx_ptr, s->win_ptr, s->stand_x, s->stand_y, RED);
-	i = -1;
-//	while (++i <= 50)
-//		draw_line(s, 250, 200 + i, RED);
-	
-	intersect(s, 1);
-	draw_line(s, s->its_x, s->its_y, RED);
 
-/*
-	i = -1;
-	while (++i <= 20)
-	{
-		intersect(s, (double)(0.05 * i));
-		draw_line(s, s->its_x, s->its_y, RED);
-	}
-*/
+	
+//	intersect(s, 0.15);
+//	draw_line(s, s->its_x, s->its_y, RED);
+
 }
